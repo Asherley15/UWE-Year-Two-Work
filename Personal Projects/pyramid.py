b3d0c97d72689600f1510d0233d3752f2ab8e8c3
Ashley <ashhh1508@gmail.com>
2021-07-30 20:17:03 +0100

    Added personal work

---
+++ Personal Projects/pyramid.py
@@ -0,0 +1,8 @@
+height = int(input("Please enter the width of the base"))
+size = height
+for i in range(height):
+    print("\n")
+    for i in range(size):
+        print('*', end=" ")
+    size -= 1
+    if size == 1:

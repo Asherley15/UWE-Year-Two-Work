1efecf0fead41d07eca5a061404ada79745706e9
Ashley <ashhh1508@gmail.com>
2021-08-02 19:47:19 +0100

    Updated Colltaz with plotting

--- Personal Projects/colltaz.py
+++ Personal Projects/colltaz.py
@@ -1,18 +1,47 @@
-from typing import Counter
+import matplotlib.pyplot as plt
+import numpy as np
 
-
+# Get Initial number
 num = float(input("Please enter your starting number:\n"))
-counter = 0
-# if number isn't 1, enter loop
 
+# Set counter to 0
+counter = 0
+
+# Create initial arrays from counter and users number
+x = np.array([counter])
+y = np.array([num])
+
+# Set program to stop once 1 has been reached, beyond this an infinite 4-2-1 loop is formed.
 while num != 1:
-    # Check if num is even
+    # Increment Counter
     counter += 1
+
+    # Add counter to X axis
+    x = np.append(x, [counter])
+
+    # If number is even (Modulo ==0) divide number by 2.
     if (num % 2 == 0):
         num = num / 2
-        print("Current number after", counter, "steps is:", num)
+        # Add number to Y Axis array
+        y = np.append(y, [num])
+
     else:
+        # If number is odd, 3x+1
         num = num * 3 + 1
-        print("Current number after", counter, "steps is:", num)
+
+        # Add number to Y Axis array
+        y = np.append(y, [num])
+
 else:
+    # Upon counter reaching 1000:
     print("Reached 1 in", counter, "steps")
+
+    # Plot X and Y arrays
+    plt.plot(x, y)
+
+    # Label x and y Axis's
+    plt.xlabel('Steps')
+    plt.ylabel('Number')
+
+    # Display Graph
+    plt.show()
---
+++ Personal Projects/graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3]
+y = [2, 4, 1]
+plt.plot(x, y)
+
+plt.xlabel('Counter')
+plt.ylabel('Y Axis')
+
+plt.title('Does this work?')
+
+plt.show()

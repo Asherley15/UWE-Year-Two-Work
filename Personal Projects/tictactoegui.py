7e00f73604390786d8b244f086606356096338f0
Ashley <ashhh1508@gmail.com>
2021-08-04 18:59:43 +0100

    Working AI tictactoe

--- Personal Projects/tictactoegui.py
+++ Personal Projects/tictactoegui.py
@@ -13,38 +13,52 @@ clicked = True
 count = 0
 
 print(grid)
-x = True
+x = False
+pctur = False
 
 
 def b_click(i, j, b):
-    global x
-    if x:
+    global pctur, x, selection, buttons
+    if pctur == False:
         if grid[i, j] != "X" and grid[i, j, ] != 'O':
             grid[i, j] = "X"
             print(grid)
             b["text"] = 'X'
             b["fg"] = "Red"
-            x = False
-        elif grid[i, j] == 'X' or grid[i, j] == 'O':
-            print("Invalid selection, choose another space")
-    if x == False:
-        if grid[i, j] != "X" and grid[i, j, ] != 'O':
-            grid[i, j] = "O"
-            print(grid)
-            b["text"] = 'O'
-            b["fg"] = "Green"
-            x = True
-            X = False
+
+            global buttons
+            buttons.remove(b)
+            pctur = True
+
         elif grid[i, j] == 'X' or grid[i, j] == 'O':
             print("Invalid selection, choose another space")
 
+    if pctur == True:
+        if x == False:
+            selection = random.choice(buttons)
+            if selection in (buttons):
+                x = True
+                selection.invoke()
 
-def pcturn():
-    random.randint(0,)
+        if x == True:
+            if grid[i, j] != "X" and grid[i, j] != 'O':
+                grid[i, j] = "X"
+                grid[i, j] = "O"
+                x = False
+                buttons.remove(b)
+                print(grid)
+                b["text"] = 'O'
+                b["fg"] = "Green"
+                x = False
+                pctur = False
+
+            elif grid[i, j] == 'X' or grid[i, j] == 'O':
+                print("It guessed.")
+                x = False
 
 
 b1 = Button(root, text=" ", font=("Helvetica", 20), height=3,
-            width=6, bg="White", fg="Red", command=lambda: b_click(0, 0, b=b1))
+            width=6, bg="White", fg="Red", command=lambda: b_click(0, 0, b1))
 b2 = Button(root, text=" ", font=("Helvetica", 20), height=3,
             width=6, bg="White", command=lambda: b_click(0, 1, b2))
 b3 = Button(root, text=" ", font=("Helvetica", 20), height=3,
@@ -61,6 +75,17 @@ b8 = Button(root, text=" ", font=("Helvetica", 20), height=3,
             width=6, bg="White", command=lambda: b_click(2, 1, b8))
 b9 = Button(root, text=" ", font=("Helvetica", 20), height=3,
             width=6, bg="White", command=lambda: b_click(2, 2, b9))
+
+buttons = [b1, b2, b3, b4, b5, b6, b7, b8, b9]
+
+
+def pcturn():
+    global buttons, selection
+    selection = random.choice(buttons)
+    if selection in (buttons):
+        selection = selection
+
+
 b1.grid(row=0, column=0)
 b2.grid(row=0, column=1)
 b3.grid(row=0, column=2)
@@ -70,6 +95,5 @@ b6.grid(row=1, column=2)
 b7.grid(row=2, column=0)
 b8.grid(row=2, column=1)
 b9.grid(row=2, column=2)
-
 
 root.mainloop()

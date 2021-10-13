18a44e94564ab1e018ced42b2bc3b740b41d5386
Ashley <ashhh1508@gmail.com>
2021-10-13 16:16:08 +0100

    Week2 prac working(no functions)

--- AI2/Week2prac.py
+++ AI2/Week2prac.py
@@ -8,6 +8,8 @@ P = 50
 
 
 class individual:
+    def __init__(self) -> None:
+        pass
     gene = []
     fitness = 0
 
@@ -16,32 +18,45 @@ population = []
 
 for x in range(0, P):
     tempgene = []
-    for x in range(0, N):
+    for y in range(0, N):
         tempgene.append(random.randint(0, 1))
     newind = individual()
     newind.gene = tempgene.copy()
     population.append(newind)
 
 
-def test_func(individual):
+def test_func(pop):
     fitness = 0
-    for x in range(0, N):
-        if individual.gene[x] == 1:
-            fitness += 1
+    for x in pop:
+        for y in x.gene:
+            if y == 1:
+                fitness += 1
 
+    print('Total fitness:', fitness)
+    return fitness
+
+
+offspring = []
 
 for i in range(0, P):
-    offarray = []
     parent1 = random.randint(0, P - 1)
     off1 = population[parent1]
     parent2 = random.randint(0, P - 1)
     off2 = population[parent2]
 
     if off1.fitness > off2.fitness:
-        offarray.append(off1)
+        offspring.append(off1)
 
     else:
-        offarray.append(off2)
-    if offarray.fitness()
+        offspring.append(off2)
+    offspring
 
-print(individual.fitness)
+
+if test_func(offspring) > test_func(population):
+    print('Breed successful')
+    population = offspring
+else:
+    print('Breed failed')
+    population = population
+
+test_func(population)

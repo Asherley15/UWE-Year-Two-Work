36329ddc7c01fe957a615a19e586342c78510adb
Ashley <ashhh1508@gmail.com>
2021-11-03 14:54:51 +0000

    Week 4 Practical, added ints instead of binary selection

--- AI2/Week4Prac.py
+++ AI2/Week4Prac.py
@@ -5,8 +5,8 @@ import numpy as np
 import copy
 import statistics
 
-N = 10
-P = 10
+N = 50
+P = 50
 fitnessscore = []
 totalfitmut = 0
 
@@ -49,118 +49,116 @@ runs = 500
 
 
 for gencheck in range(0, runs):
-    if popbestscore != 50:
-        popscorelist = []
-        genz += 1
+    popscorelist = []
+    genz += 1
+    counter = -1
+    for x in population:
+        counter += 1
+        population[counter].fitness = test_func(x)
+
+    offspring = []
+    for i in range(0, P):
+        parent1 = random.randint(0, P - 1)
+        off1 = population[parent1]
+        parent2 = random.randint(0, P - 1)
+        off2 = population[parent2]
+        if off1.fitness > off2.fitness:
+            offspring.append(off1)
+
+        else:
+            offspring.append(off2)
         counter = -1
-        for x in population:
-            counter += 1
-            population[counter].fitness = test_func(x)
 
-        offspring = []
-        for i in range(0, P):
-            parent1 = random.randint(0, P - 1)
-            off1 = population[parent1]
-            parent2 = random.randint(0, P - 1)
-            off2 = population[parent2]
-            if off1.fitness > off2.fitness:
-                offspring.append(off1)
+    for x in offspring:
+        counter += 1
+        offspring[counter].fitness = test_func(x)
+    totalfitpop = 0
+    totalfitoff = 0
 
-            else:
-                offspring.append(off2)
-            counter = -1
+    counter = -1
+    for x in population:
+        counter += 1
+        totalfitpop += population[counter].fitness
+    counter = -1
+    for x in offspring:
+        counter += 1
+        totalfitoff += offspring[counter].fitness
 
-        for x in offspring:
-            counter += 1
-            offspring[counter].fitness = test_func(x)
-        totalfitpop = 0
-        totalfitoff = 0
+    crosspoint = random.randint(0, N - 1)
+    z = 0
 
-        counter = -1
-        for x in population:
-            counter += 1
-            totalfitpop += population[counter].fitness
-        counter = -1
-        for x in offspring:
-            counter += 1
-            totalfitoff += offspring[counter].fitness
+    for i in range(0, P, 2):
 
-        crosspoint = random.randint(0, N - 1)
-        z = 0
+        tempgene = offspring[i].gene.copy()
 
-        for i in range(0, P, 2):
+        for k in range(0, crosspoint):
+            offspring[i].gene[k] = offspring[i + 1].gene[k]
+            offspring[i + 1].gene[k] = tempgene[k]
 
-            tempgene = offspring[i].gene.copy()
-
-            for k in range(0, crosspoint):
-                offspring[i].gene[k] = offspring[i + 1].gene[k]
-                offspring[i + 1].gene[k] = tempgene[k]
-
-        mutatedgenes = []
-        mutrate = 0.01
-        mutcheck = 0
-        total = 0
-        for i in range(0, P):
-            newind = individual()
-            newind.gene = []
-            for j in range(0, N):
-                mutprob = random.random()
-                gene = offspring[i].gene[j]
-                total += 1
-                if (mutprob) < (mutrate):
-                    if (gene == 1):
-                        gene = 0
-                    else:
+    mutatedgenes = []
+    mutrate = 0.0275
+    mutcheck = 0
+    alter = random.random()
+    flip = 0.5
+    for i in range(0, P):
+        newind = individual()
+        newind.gene = []
+        for j in range(0, N):
+            mutprob = random.random()
+            gene = offspring[i].gene[j]
+            if (mutprob) < (mutrate):
+                if random.random() < flip:
+                    gene += alter
+                    if gene > 1:
                         gene = 1
-                newind.gene.append(gene)
-            mutatedgenes.append(newind)
+                else:
+                    gene -= alter
+            newind.gene.append(gene)
+        mutatedgenes.append(newind)
 
-        totalfitmut = 0
-        counter = -1
+    totalfitmut = 0
+    counter = -1
 
-        for x in mutatedgenes:
-            counter += 1
-            totalfitmut += test_func(x)
+    for x in mutatedgenes:
+        counter += 1
+        totalfitmut += test_func(x)
 
-        bestbaby = individual()
-        bestbaby = population[0]
-        for x in population:
-            if x.fitness > bestbaby.fitness:
-                bestbaby = x
+    bestbaby = individual()
+    bestbaby = population[0]
+    for x in population:
+        if x.fitness > bestbaby.fitness:
+            bestbaby = x
 
-        population = copy.deepcopy(mutatedgenes)
+    population = copy.deepcopy(mutatedgenes)
 
-        worsebaby = individual()
-        worsebaby = population[0]
-        for x in population:
-            if x.fitness < worsebaby.fitness:
-                worsebaby = x
-        worsebaby = bestbaby
-        fitnessscore.append(totalfitmut)
+    worsebaby = individual()
+    worsebaby = population[0]
+    for x in population:
+        if x.fitness < worsebaby.fitness:
+            worsebaby = x
+    worsebaby = bestbaby
+    fitnessscore.append(totalfitmut)
 
-        counter = -1
-        for x in population:
-            counter += 1
-            population[counter].fitness = test_func(x)
+    counter = -1
+    for x in population:
+        counter += 1
+        population[counter].fitness = test_func(x)
 
-        for x in population:
-            popscorelist.append(x.fitness)
-            if x.fitness >= popbestscore:
-                popbestscore = x.fitness
+    for x in population:
+        popscorelist.append(x.fitness)
+        if x.fitness >= popbestscore:
+            popbestscore = x.fitness
 
-        yaxis = np.append(yaxis, popbestscore)
+    yaxis = np.append(yaxis, popbestscore)
 
-        avgpopscore = statistics.mean(popscorelist)
-        avgscorelist = np.append(avgscorelist, avgpopscore)
-        generation = []
-        generation.append(gencheck)
+    avgpopscore = statistics.mean(popscorelist)
+    avgscorelist = np.append(avgscorelist, avgpopscore)
+    generation = []
+    generation.append(gencheck)
 
-        xaxis = np.append(xaxis, [genz])
-        for x in population:
-            print(x.gene)
-        plt.plot(xaxis, yaxis, avgscorelist)
-    else:
-        break
+    xaxis = np.append(xaxis, [genz])
+
+    plt.plot(xaxis, yaxis, avgscorelist)
 
 z = 0
 
